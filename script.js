c8104a6648bef28c9bda4afb84e13ad62aea124e
NahidulNoman <nahidulna5@gmail.com>
2022-09-15 00:04:57 +0600

    errorCount-show-and-responsive

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ const typeController = (e) => {
   if (newLetterCorrect) {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
+    errorCount += newLetter.length
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
   }
 
